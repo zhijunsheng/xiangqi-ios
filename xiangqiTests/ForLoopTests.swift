fb89022ccd29ca2f599fa57e5797712192f8ef6e
Zhijun Sheng <zhijunsheng@gmail.com>
2018-04-03 10:02:46 -0400

    cleaned up for the next new-comer

---
+++ xiangqiTests/ForLoopTests.swift
@@ -0,0 +1,20 @@
+//
+//  ForLoopTests.swift
+//  xiangqiTests
+//
+//  Created by Zhijun Sheng on 2018-03-04.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+
+class ForLoopTests: XCTestCase {
+    
+    func testPriniting() {
+        for i in 0...5 {
+            print("i is \(i)")
+        }
+    }
+    
+}
+
