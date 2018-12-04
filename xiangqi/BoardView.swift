c2efd7efd1020fb88ce4aea08768be763bd7ed07
Peter Shi <petershi@Peters-MacBook-Pro.local>
2018-12-04 11:09:33 -0500

    killed a lot of duplicated code

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -13,6 +13,8 @@ class BoardView: UIView {
     let originX: CGFloat = 6.0
     let originY: CGFloat = 6.0
     let side: CGFloat = 30.0
+    let space: CGFloat = 3.0
+    let line: CGFloat = 9.0
     
     override func draw(_ rect: CGRect) {
         
@@ -36,6 +38,10 @@ class BoardView: UIView {
             pen.addLine(to: CGPoint(x: originX + side * CGFloat(i), y: originY + side * 9))
         }
 
+         ////\\\\
+        /////\\\\\
+//       |  |
+//
         // diagonal
         pen.move(to: CGPoint(x: originX + side * 3, y: originY + side * 0))
         pen.addLine(to: CGPoint(x: originX + side * 5, y: originY + side * 2))
@@ -49,47 +55,22 @@ class BoardView: UIView {
         pen.move(to: CGPoint(x: originX + side * 5, y: originY + side * 7))
         pen.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 9))
         
+        UIColor.black.setStroke()
+        pen.lineWidth = 2
         
+        pen.stroke()
         
         for i in 0..<8 {
             if i % 2 == 0 {
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY - 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 12) + side * 3))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY + 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 12) + side * 3))
-                
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY - 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 12) + side * 6))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY + 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 12) + side * 6))
+                drawStar(row: 3, col: i, drawingUtensil: pen)
+                drawStar(row: 6, col: i, drawingUtensil: pen)
             }
         }
         
         for i in 2..<9 {
             if i % 2 == 0 {
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 12) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY - 3) + side * 3))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 12) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 3) + side * 3))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY + 3) + side * 3))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 12) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY - 3) + side * 6))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 12) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 3) + side * 6))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY + 3) + side * 6))
-                
+                drawStar(row: 3, col: i, drawingUtensil: pen)
+                drawStar(row: 6, col: i, drawingUtensil: pen)
             }
         }
         
@@ -97,48 +78,45 @@ class BoardView: UIView {
         //💣
         for i in 0...9 {
             if i == 1 || i == 7 {
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY - 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 12) + side * 2))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY + 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 12) + side * 2))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 12) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY - 3) + side * 2))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 12) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 3) + side * 2))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY + 3) + side * 2))
+                drawStar(row: 2, col: i, drawingUtensil: pen)
             }
         }
         
 
         for i in 0...9 {
             if i == 1 || i == 7 {
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY - 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY - 12) + side * 7))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) + 12, y: (originY + 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) + 3, y: (originY + 12) + side * 7))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 12) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY - 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY - 3) + side * 7))
-                
-                pen.move(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 12) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 3, y: (originY + 3) + side * 7))
-                pen.addLine(to: CGPoint(x: originX + side * CGFloat(i) - 12, y: (originY + 3) + side * 7))
+                drawStar(row: 7, col: i, drawingUtensil: pen)
             }
         }
         
         UIColor.black.setStroke()
-        pen.lineWidth = 2
+        pen.lineWidth = 1.25
         
         pen.stroke()
+    }
+    
+    func drawStar(row: Int, col: Int, drawingUtensil: UIBezierPath) {
+        if col == 0 {
+            leftRight(row: row, col: col, drawingUtensil: drawingUtensil, isLeft: false)
+        } else if col == 8 {
+            leftRight(row: row, col: col, drawingUtensil: drawingUtensil, isLeft: true)
+        } else {
+            leftRight(row: row, col: col, drawingUtensil: drawingUtensil, isLeft: true)
+            leftRight(row: row, col: col, drawingUtensil: drawingUtensil, isLeft: false)
+        }
+    }
+    
+    func leftRight(row: Int, col: Int, drawingUtensil: UIBezierPath, isLeft: Bool) {
+        var sign: CGFloat = 1
+        if isLeft {
+            sign = -1
+        }
+        drawingUtensil.move(to: CGPoint(x: originX + side * CGFloat(col) + sign * space, y: (originY - line) + side *  CGFloat(row)))
+        drawingUtensil.addLine(to: CGPoint(x: originX + side * CGFloat(col) + sign * space, y: (originY - space) + side *  CGFloat(row)))
+        drawingUtensil.addLine(to: CGPoint(x: originX + side * CGFloat(col) + sign * line, y: (originY - space) + side *  CGFloat(row)))
+        
+        drawingUtensil.move(to: CGPoint(x: originX + side * CGFloat(col) + sign * space, y: (originY + line) + side *  CGFloat(row)))
+        drawingUtensil.addLine(to: CGPoint(x: originX + side * CGFloat(col) + sign * space, y: (originY + space) + side *  CGFloat(row)))
+        drawingUtensil.addLine(to: CGPoint(x: originX + side * CGFloat(col) + sign * line, y: (originY + space) + side *  CGFloat(row)))
     }
 }
