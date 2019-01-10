a203ecc79dc69ae93dc517706c1df216b577926a
Peter Shi <petershi@Peters-MacBook-Pro.local>
2019-01-09 21:55:48 -0500

    added a panGestureRecognizer.

--- xiangqi/ViewController.swift
+++ xiangqi/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     var boardY: CGFloat = 0
     
     @IBOutlet weak var boardView: BoardView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         
         print(boardView)
         
+        //black
+     // regular pieces
         addPiece(image: #imageLiteral(resourceName: "bj"), row: 0, col: 0)
         addPiece(image: #imageLiteral(resourceName: "bm"), row: 0, col: 1)
         addPiece(image: #imageLiteral(resourceName: "bx"), row: 0, col: 2)
@@ -30,16 +33,19 @@ class ViewController: UIViewController {
         addPiece(image: #imageLiteral(resourceName: "bm"), row: 0, col: 7)
         addPiece(image: #imageLiteral(resourceName: "bj"), row: 0, col: 8)
         
+     // c= *  *  *
         addPiece(image: #imageLiteral(resourceName: "bp"), row: 2, col: 1)
         addPiece(image: #imageLiteral(resourceName: "bp"), row: 2, col: 7)
-        
+     
+     //
         addPiece(image: #imageLiteral(resourceName: "bz"), row: 3, col: 0)
         addPiece(image: #imageLiteral(resourceName: "bz"), row: 3, col: 2)
         addPiece(image: #imageLiteral(resourceName: "bz"), row: 3, col: 4)
         addPiece(image: #imageLiteral(resourceName: "bz"), row: 3, col: 6)
         addPiece(image: #imageLiteral(resourceName: "bz"), row: 3, col: 8)
         
-        
+        //red
+     // regular pieces
         addPiece(image: #imageLiteral(resourceName: "rj"), row: 9, col: 0)
         addPiece(image: #imageLiteral(resourceName: "rm"), row: 9, col: 1)
         addPiece(image: #imageLiteral(resourceName: "rx"), row: 9, col: 2)
@@ -50,15 +56,24 @@ class ViewController: UIViewController {
         addPiece(image: #imageLiteral(resourceName: "rm"), row: 9, col: 7)
         addPiece(image: #imageLiteral(resourceName: "rj"), row: 9, col: 8)
         
+     // c=__*__*__*__*__*__*__*__*__*__*__*__*_______________________________________________________________________
         addPiece(image: #imageLiteral(resourceName: "rp"), row: 7, col: 1)
         addPiece(image: #imageLiteral(resourceName: "rp"), row: 7, col: 7)
         
+     //
         addPiece(image: #imageLiteral(resourceName: "rz"), row: 6, col: 0)
         addPiece(image: #imageLiteral(resourceName: "rz"), row: 6, col: 2)
         addPiece(image: #imageLiteral(resourceName: "rz"), row: 6, col: 4)
         addPiece(image: #imageLiteral(resourceName: "rz"), row: 6, col: 6)
         addPiece(image: #imageLiteral(resourceName: "rz"), row: 6, col: 8)
         
+    }
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        let uncheckedCol0 = (sender.location(in: boardView).x - boardView.originX) / boardView.side
+        let uncheckedRow0 = (sender.location(in: boardView).y - boardView.originY) / boardView.side
+        let col = Int(uncheckedCol0) + (uncheckedCol0 < floor(uncheckedCol0) + 0.5 ? 0 : 1)
+        let row = Int(uncheckedRow0) + (uncheckedRow0 < floor(uncheckedRow0) + 0.5 ? 0 : 1)
+        print("row is........................\(row)!, and col is.........................\(col)!")
     }
     
     func addPiece(image: UIImage, row: Int, col: Int) {
