efcbac646282449e8576eb0b037f5006ab55b2aa
ethan <ethanyiyaozhang@gmail.com>
2019-03-20 19:05:01 +0800

    drew almost a board

---
+++ xiangqi/BoardView.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class BoardView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let pen = UIBezierPath()
+
+        for i in 0..<10 {
+            pen.move(to: CGPoint(x: 10, y: i * 31 + 10))
+            pen.addLine(to: CGPoint(x: 8 * 31 + 10, y: i * 31 + 10))
+
+        }
+        
+        for i in 0..<12 {
+            pen.move(to: CGPoint(x: i * 31 + 10, y: 10))
+            pen.addLine(to: CGPoint(x: i * 31 + 10, y: 4 * 31 + 10))
+            
+        }
+        
+        for i in 0..<12 {
+            pen.move(to: CGPoint(x: i * 31 + 10, y: 5 * 31 + 10))
+            pen.addLine(to: CGPoint(x: i * 31 + 10, y: 9 * 31 + 10))
+            
+        }
+        
+        pen.move(to: CGPoint(x: 3 * 31 + 10, y: 10))
+        pen.addLine(to: CGPoint(x: 5 * 31 + 10, y: 2 * 31 + 10))
+        
+        pen.move(to: CGPoint(x: 5 * 31 + 10, y: 10))
+        pen.addLine(to: CGPoint(x: 3 * 31 + 10, y: 2 * 31 + 10))
+        
+        pen.move(to: CGPoint(x: 3 * 31 + 10, y: 7 * 31 + 10))
+        pen.addLine(to: CGPoint(x: 5 * 31 + 10, y: 9 * 31 + 10))
+        
+        pen.move(to: CGPoint(x: 5 * 31 + 10, y: 7 * 31 + 10))
+        pen.addLine(to: CGPoint(x: 3 * 31 + 10, y: 9 * 31 + 10))
+            
+  
+        
+        pen.stroke()
+        
+    }
+   
+
+}
