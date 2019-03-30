2e54cc4fb3e9a89049b03413e05e61a62da1ca09
ethan <ethanyiyaozhang@gmail.com>
2019-03-30 19:49:25 +0800

    we drew a frame

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -2,9 +2,11 @@ import UIKit
 
 class BoardView: UIView {
     
-    let originX: CGFloat = 10
-    let originY: CGFloat = 10
-    let cellSide: CGFloat = 31
+    let originX: CGFloat = 30
+    let originY: CGFloat = 30
+    let cellSide: CGFloat = 29
+    let gap: CGFloat = 20
+    
     
     // refactored 重构
     override func draw(_ rect: CGRect) {
@@ -13,37 +15,45 @@ class BoardView: UIView {
         for i in 0..<10 {
             pen.move(to: CGPoint(x: originX, y: CGFloat(i) * cellSide + originY))
             pen.addLine(to: CGPoint(x: 8 * cellSide + originX, y: CGFloat(i) * cellSide + originY))
-
         }
-        
+
         for i in 0..<9 {
             pen.move(to: CGPoint(x: CGFloat(i) * cellSide + originX, y: originY))
             pen.addLine(to: CGPoint(x: CGFloat(i) * cellSide + originX, y: 4 * cellSide + originY))
-            
         }
-        
+
         for i in 0..<9 {
             pen.move(to: CGPoint(x: CGFloat(i) * cellSide + originX, y: 5 * cellSide + originY))
             pen.addLine(to: CGPoint(x: CGFloat(i) * cellSide + originX, y: 9 * cellSide + originY))
-            
         }
+        
+        // 2 Xs
         
         pen.move(to: CGPoint(x: 3 * cellSide + originX, y: originY))
         pen.addLine(to: CGPoint(x: 5 * cellSide + originX, y: 2 * cellSide + originY))
-        
+
         pen.move(to: CGPoint(x: 5 * cellSide + originX, y: originY))
         pen.addLine(to: CGPoint(x: 3 * cellSide + originX, y: 2 * cellSide + originY))
-        
+
         pen.move(to: CGPoint(x: 3 * cellSide + originX, y: 7 * cellSide + originY))
         pen.addLine(to: CGPoint(x: 5 * cellSide + originX, y: 9 * cellSide + originY))
-        
+
         pen.move(to: CGPoint(x: 5 * cellSide + originX, y: 7 * cellSide + originY))
         pen.addLine(to: CGPoint(x: 3 * cellSide + originX, y: 9 * cellSide + originY))
+
+        // river
+
+        pen.move(to: CGPoint(x: originX, y: originY + 4 * cellSide))
+        pen.addLine(to: CGPoint(x: originX, y: originY + 5 * cellSide))
         
-        pen.move(to: CGPoint(x: originX, y: 4 * cellSide + originY))
-        pen.addLine(to: CGPoint(x: originX, y: 5 * cellSide + originY))
+        pen.move(to: CGPoint(x: originX + 8 * cellSide, y: originY + 4 * cellSide))
+        pen.addLine(to: CGPoint(x: originX + 8 * cellSide, y: originY + 5 * cellSide))
+        
+        pen.lineWidth = 0.5
         
         pen.stroke()
+        
+        drawFrame()
         
         drawFlower(flowerX: 1, flowerY: 2)
         drawFlower(flowerX: 6, flowerY: 6)
@@ -84,6 +94,7 @@ class BoardView: UIView {
         pen.move(to: CGPoint(x: CGFloat(flowerX) * cellSide + originX + flowerGap, y: CGFloat(flowerY) * cellSide + originY + flowerLength))
         pen.addLine(to: CGPoint(x: CGFloat(flowerX) * cellSide + originX + flowerGap, y: CGFloat(flowerY) * cellSide + originY + flowerGap))
         
+        pen.lineWidth = 0.5
         pen.stroke()
     }
     
@@ -106,6 +117,7 @@ class BoardView: UIView {
         pen.move(to: CGPoint(x: CGFloat(flowerX) * cellSide + originX - flowerLength, y: CGFloat(flowerY) * cellSide + originY + flowerGap))
         pen.addLine(to: CGPoint(x: CGFloat(flowerX) * cellSide + originX - flowerGap, y: CGFloat(flowerY) * cellSide + originY + flowerGap))
         
+        pen.lineWidth = 0.5
         pen.stroke()
     }
    
@@ -113,4 +125,26 @@ class BoardView: UIView {
         drawLeftHalfFlower(flowerX: flowerX, flowerY: flowerY)
         drawRightHalfFlower(flowerX: flowerX, flowerY: flowerY)
     }
+    
+    func drawFrame() {
+        let pen = UIBezierPath()
+        
+        pen.move(to: CGPoint(x: originX - gap, y: originY - gap))
+        pen.addLine(to: CGPoint(x: originX + 8 * cellSide + gap, y: originY - gap))
+        
+        pen.move(to: CGPoint(x: originX + 8 * cellSide + gap, y: originY - gap))
+        pen.addLine(to: CGPoint(x: originX + 8 * cellSide + gap, y: originY + 9 * cellSide + gap))
+        
+        pen.move(to: CGPoint(x: originX - gap, y: originY - gap))
+        pen.addLine(to: CGPoint(x: originX - gap, y: originY + 9 * cellSide + gap))
+        
+        pen.move(to: CGPoint(x: originX - gap, y: originY + 9 * cellSide + gap))
+        pen.addLine(to: CGPoint(x: originX + 8 * cellSide + gap, y: originY + 9 * cellSide + gap))
+        
+        pen.lineWidth = 3
+        
+        pen.stroke()
+        
+    }
+    
 }
