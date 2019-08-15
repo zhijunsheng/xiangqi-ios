4d9289c2cdc205d4c072112fdfb8f0da3f41c79f
Hal Wang <halhaoranwang@gmail.com>
2019-08-15 09:03:04 +0800

    we added the whose turn

--- xiangqi/ViewController.swift
+++ xiangqi/ViewController.swift
@@ -10,24 +10,37 @@ import UIKit
 
 class ViewController: UIViewController, XiangqiDelegate {
     
-    func movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int) {
-        board.movePiece(fromCol: fromCol, fromRow: fromRow, toCol: toCol, toRow: toRow)
-        boardView.pieces = board.pieces
-        boardView.setNeedsDisplay()
-        
-    }
-    
     @IBOutlet weak var boardView: BoardView!
+    @IBOutlet weak var whoseTurnLabel: UILabel!
     var board = XiangqiBoard()
     var fromCol: Int? = nil
     var fromRow: Int? = nil
+    var isRedTurn: Bool = true
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        whoseTurnLabel.text = "Red's Turn"
         board.initializeBoard()
         boardView.pieces = board.pieces
         boardView.xiangqiDelegate = self
     }
+    
+    func movePiece(fromCol: Int, fromRow: Int, toCol: Int, toRow: Int) {
+        if board.pieceAt(col: fromCol, row: fromRow)?.isRed != isRedTurn {
+            return
+        }
+        
+        isRedTurn = !isRedTurn
+        board.movePiece(fromCol: fromCol, fromRow: fromRow, toCol: toCol, toRow: toRow)
+        boardView.pieces = board.pieces
+        boardView.setNeedsDisplay()
+        if isRedTurn == true {
+            whoseTurnLabel.text = "Red's Turn"
+        } else {
+            whoseTurnLabel.text = "Black's Turn"
+        }
+    }
 }
-
