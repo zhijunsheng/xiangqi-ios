56287a4bcbe9d19f0ac8fe586f9339e4fcacf26d
xiaobao <mariuspuzhouli@gmail.com>
2019-08-18 10:05:06 +0800

    wed changedd thed colord ofd Gridd andd stard

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -13,7 +13,15 @@ class BoardView   : UIView {
     var fromCol   : Int     = -10
     var fromRow   : Int     = -10
     
-    
+    override func draw(_ rect: CGRect) {
+        originY = (bounds.height - CGFloat(rows - 1) * height) / 2
+        originX = (bounds.width - CGFloat(cols - 1) * width) / 2
+        print(bounds.width)
+        print(bounds.height)
+        
+        drawStars()
+        drawGrid()
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touchOne = touches.first!
@@ -36,70 +44,72 @@ class BoardView   : UIView {
         print("from: (\(fromCol), \(fromRow)) to: (\(toCol), \(toRow))")
     }
     
-    override func draw(_ rect: CGRect) {
-        originY = (bounds.height - CGFloat(rows - 1) * height) / 2
-        originX = (bounds.width - CGFloat(cols - 1) * width) / 2
-        print(bounds.width)
-        print(bounds.height)
+    func drawGrid() {
+        let b2 = UIBezierPath()
         for i in 0..<rows {
-            let b2 = UIBezierPath()
+            
             b2.move(to: CGPoint(x: originX, y: height * CGFloat(i) + originY))
             b2.addLine(to: CGPoint(x: width * CGFloat(cols - 1) + originX, y: height * CGFloat(i) + originY))
             b2.stroke()
         }
         for i in 0..<2  {
-            let c3 = UIBezierPath()
-            c3.move(to: CGPoint(x: originX + CGFloat(i) * CGFloat(cols - 1) * width, y: originY))
-            c3.addLine(to: CGPoint(x: originX + CGFloat(i) * CGFloat(cols - 1) * width, y: height * CGFloat(rows - 1) + originY))
-            c3.stroke()
+            
+            b2.move(to: CGPoint(x: originX + CGFloat(i) * CGFloat(cols - 1) * width, y: originY))
+            b2.addLine(to: CGPoint(x: originX + CGFloat(i) * CGFloat(cols - 1) * width, y: height * CGFloat(rows - 1) + originY))
+            b2.stroke()
         }
         for i in 1...7  {
-            let d4 = UIBezierPath()
-            d4.move(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: originY))
-            d4.addLine(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * 4 + originY))
-            d4.stroke()
+            
+            b2.move(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: originY))
+            b2.addLine(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * 4 + originY))
+            b2.stroke()
         }
         for i in 1...7  {
-            let d4 = UIBezierPath()
-            d4.move(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * 5 + originY))
-            d4.addLine(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * CGFloat(rows - 1) + originY))
-            d4.stroke()
+            
+            b2.move(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * 5 + originY))
+            b2.addLine(to: CGPoint(x: originX + CGFloat(i) * 1 * width, y: height * CGFloat(rows - 1) + originY))
+            b2.stroke()
         }
         
-        let e5 = UIBezierPath()
-        e5.move(to: CGPoint(x: width * 3 + originX, y: originY))
-        e5.addLine(to: CGPoint(x: width * 5 + originX, y: height * 2 + originY))
-        e5.stroke()
+        
+        b2.move(to: CGPoint(x: width * 3 + originX, y: originY))
+        b2.addLine(to: CGPoint(x: width * 5 + originX, y: height * 2 + originY))
+        b2.stroke()
         
         // ================================================
         
-        let f6 = UIBezierPath()
-        f6.move(to: CGPoint(x: width * 5 + originX, y: originY))
-        f6.addLine(to: CGPoint(x: width * 3 + originX, y: height * 2 + originY))
-        f6.stroke()
+      
+        b2.move(to: CGPoint(x: width * 5 + originX, y: originY))
+        b2.addLine(to: CGPoint(x: width * 3 + originX, y: height * 2 + originY))
+        
         
         // =================================================
         
-        let g7 = UIBezierPath()
-        g7.move(to: CGPoint(x: width * 3 + originX, y: height * 7 + originY))
-        g7.addLine(to: CGPoint(x: width * 5 + originX, y: height * 9 + originY))
-        g7.stroke()
+        
+        b2.move(to: CGPoint(x: width * 3 + originX, y: height * 7 + originY))
+        b2.addLine(to: CGPoint(x: width * 5 + originX, y: height * 9 + originY))
+        
         
         // =================================================
         
-        let h8 = UIBezierPath()
-        h8.move(to: CGPoint(x: width * 5 + originX, y: height * 7 + originY))
-        h8.addLine(to: CGPoint(x: width * 3 + originX, y: height * 9 + originY))
-        h8.stroke()
+        
+        b2.move(to: CGPoint(x: width * 5 + originX, y: height * 7 + originY))
+        b2.addLine(to: CGPoint(x: width * 3 + originX, y: height * 9 + originY))
+       
         
         // =================================================
         
-        let j10 = UIBezierPath()
-        j10.move(to: CGPoint(x: originX, y: originY + height * 4.5))
-        j10.addLine(to: CGPoint(x: originX + width * 8, y: originY + height * 4.5))
+        
+        b2.move(to: CGPoint(x: originX, y: originY + height * 4.5))
+        b2.addLine(to: CGPoint(x: originX + width * 8, y: originY + height * 4.5))
         #colorLiteral(red: 0, green: 0.6235294118, blue: 0.9176470588, alpha: 1).setStroke()
-        j10.stroke()
-
+        b2.stroke()
+        
+        
+        
+    }
+    
+    func drawStars() {
         drawStar(pointX : width + originX, pointY : height + height + originY)
         drawStar(pointX : width * 2 + originX, pointY : height * 3 + originY)
         drawStar(pointX : width * 4 + originX, pointY : height * 3 + originY)
@@ -117,8 +127,8 @@ class BoardView   : UIView {
         
         drawRightStar(pointX : originX, pointY : height * 3 + originY)
         drawRightStar(pointX : originX, pointY : height * 6 + originY)
-        
     }
+    
     
     func drawStar(pointX : CGFloat, pointY : CGFloat) {
         let j10 = UIBezierPath()
@@ -141,6 +151,7 @@ class BoardView   : UIView {
         j10.move(to: CGPoint(x: -gapX + -shortLine + pointX, y: -gapY + pointY))
         j10.addLine(to: CGPoint(x: -gapX + pointX, y: -gapY + pointY))
         j10.addLine(to: CGPoint(x: -gapX + pointX, y: -gapY + -shortLine + pointY))
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         j10.stroke()
     }
     
@@ -156,6 +167,7 @@ class BoardView   : UIView {
         j10.move(to: CGPoint(x: -gapX + -shortLine + pointX, y: -gapY + pointY))
         j10.addLine(to: CGPoint(x: -gapX + pointX, y: -gapY + pointY))
         j10.addLine(to: CGPoint(x: -gapX + pointX, y: -gapY + -shortLine + pointY))
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         j10.stroke()
     }
         
@@ -171,6 +183,7 @@ class BoardView   : UIView {
         j10.move(to: CGPoint(x: gapX + shortLine + pointX, y: gapY + pointY))
         j10.addLine(to: CGPoint(x: gapX + pointX, y: gapY + pointY))
         j10.addLine(to: CGPoint(x: gapX + pointX, y: gapY + shortLine + pointY))
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         j10.stroke()
     }
 }
