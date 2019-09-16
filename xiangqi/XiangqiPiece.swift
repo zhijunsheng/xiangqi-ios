c4231d7fc6806213fc2bf2bf533c222767022ffc
ethan <ethanyiyaozhang@gmail.com>
2019-09-16 20:59:01 +0800

    We created XiangqiPiece.

---
+++ xiangqi/XiangqiPiece.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct XiangqiPiece {
+    let col: Int
+    let row: Int
+    let isRed: Bool
+    let rank: String //"r", "n", "b", "w", "k", "c", "p".
+    let imageName: String
+    
+}
