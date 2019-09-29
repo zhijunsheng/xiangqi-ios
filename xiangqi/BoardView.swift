b5cc2e3d926e3d253e62f620b4d215980efb6ad5
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-09-28 20:15:08 -0400

    I created a canvas for a chinese chessboard with auto layout!

---
+++ xiangqi/BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  xiangqi
+//
+//  Created by Angie Yan on 2019-09-28.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+  
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+ 
+
+}
