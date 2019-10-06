4dc296cd8678ab4f6180ce971dd01030c8893d76
Andy Yan <andy@Andys-MacBook-Air.local>
2019-10-06 10:04:11 -0400

    we finished our chinese chess board

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -12,16 +12,18 @@ class BoardView: UIView {
     
     let originX: CGFloat = 100
     let originY: CGFloat = 50
-    let cellSide: CGFloat = 50
+    let cellSide: CGFloat = 62.5
     
     
     override func draw(_ rect: CGRect) {
         
         let pencil = UIBezierPath()
         
+        //      Horizantal Lines 1
+        
         pencil.move(to: CGPoint (x: originX, y: originY))
         pencil.addLine(to: CGPoint (x: 600, y: originY))
-        
+
         pencil.move(to: CGPoint (x: originX, y: originY + cellSide))
         pencil.addLine(to: CGPoint (x: 600, y: originY + cellSide))
         
@@ -49,24 +51,71 @@ class BoardView: UIView {
         pencil.move(to: CGPoint (x: originX, y: originY + cellSide * 9))
         pencil.addLine(to: CGPoint (x: 600, y: originY + cellSide * 9))
         
+        //  Upper Vertical Lines
+        
         pencil.move(to: CGPoint (x: originX, y: originY))
         pencil.addLine(to: CGPoint (x: originX, y: originY + cellSide * 9))
         
-        pencil.move(to: CGPoint (x: 600, y: originY))
-        pencil.addLine(to: CGPoint (x: 600, y: originY + cellSide * 9))
-
         pencil.move(to: CGPoint (x: originX + cellSide, y: originY))
         pencil.addLine(to: CGPoint (x: originX + cellSide, y: originY + cellSide * 4))
 
         pencil.move(to: CGPoint (x: originX + cellSide * 2, y: originY))
         pencil.addLine(to: CGPoint (x: originX + cellSide * 2, y: originY + cellSide * 4))
 
+        pencil.move(to: CGPoint (x: originX + cellSide * 3, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 3, y: originY + cellSide * 4))
         
+        pencil.move(to: CGPoint (x: originX + cellSide * 4, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 4, y: originY + cellSide * 4))
         
+        pencil.move(to: CGPoint (x: originX + cellSide * 5, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 5, y: originY + cellSide * 4))
         
+        pencil.move(to: CGPoint (x: originX + cellSide * 6, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 6, y: originY + cellSide * 4))
         
+        pencil.move(to: CGPoint (x: originX + cellSide * 7, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 7, y: originY + cellSide * 4))
         
+        pencil.move(to: CGPoint (x: 600, y: originY))
+        pencil.addLine(to: CGPoint (x: 600, y: originY + cellSide * 9))
         
+        // Lower Vertical Lines
+        
+        pencil.move(to: CGPoint (x: originX + cellSide , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 2 , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 2, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 3 , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 3, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 4 , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 4, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 5 , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 5, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 6 , y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 6, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 7, y: originY + cellSide * 5))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 7, y: originY + cellSide * 9))
+        
+        //Upper Cross Line
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 3, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 5, y: originY + cellSide * 2))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 5, y: originY))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 3, y: originY + cellSide * 2))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 3, y: originY + cellSide * 7))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 5, y: originY + cellSide * 9))
+        
+        pencil.move(to: CGPoint (x: originX + cellSide * 5,  y: originY + cellSide * 7))
+        pencil.addLine(to: CGPoint (x: originX + cellSide * 3, y: originY + cellSide * 9))
         
         pencil.stroke()
         
