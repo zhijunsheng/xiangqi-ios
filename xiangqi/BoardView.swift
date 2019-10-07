5ac76791514c08dda965f6cd5f5b5cea6027d16f
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-10-06 20:21:27 -0500

    added a piece and made a loop for the two X'sX

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -12,23 +12,43 @@ class BoardView: UIView {
     
     let originX:CGFloat = 25
     let originY:CGFloat = 25
-    let boardWidth:CGFloat = 258
-   
+    let cellSide:CGFloat = 258/8
     override func draw(_ rect: CGRect) {
+        let pieceImage = UIImage(named:"rb")
+        pieceImage?.draw(at: CGPoint(x: 25, y: 25))
+        
         let pen = UIBezierPath()
         pen.move(to: CGPoint(x: originX, y: originY))
-        pen.addLine(to: CGPoint(x: boardWidth + originX, y: originY))
-        pen.addLine(to: CGPoint(x: boardWidth + originX, y: 300))
-        pen.addLine(to: CGPoint(x: originX, y: 300))
+        pen.addLine(to: CGPoint(x: cellSide * 8 + originX, y: originY))
+        pen.addLine(to: CGPoint(x: cellSide * 8 + originX, y: cellSide * 9 + originY))
+        pen.addLine(to: CGPoint(x: originX, y: cellSide * 9 + originY))
         pen.close()
-        
+
         for i in 0..<7 {
-            pen.move(to: CGPoint(x: boardWidth/8.0 * (CGFloat(i) + 1) + originX, y: originY))
-            pen.addLine(to: CGPoint(x: boardWidth/8.0 * (CGFloat(i) + 1) + originX, y: 300))
-         
+            pen.move(to: CGPoint(x: cellSide * CGFloat(i + 1) + originX, y: originY))
+            pen.addLine(to: CGPoint(x: cellSide * CGFloat(i + 1) + originX, y: cellSide * 4 + originY))
+
+            pen.move(to: CGPoint(x: cellSide * CGFloat(i + 1) + originX, y: cellSide * 5 + originY))
+            pen.addLine(to: CGPoint(x: cellSide * CGFloat(i + 1) + originX, y: cellSide * 9 + originY))
 
         }
+        pen.move(to: CGPoint(x: originX, y: cellSide * 4 + originY))
+        pen.addLine(to: CGPoint(x: cellSide * 8 + originX, y: cellSide * 4 + originY))
+        pen.move(to: CGPoint(x: originX, y: cellSide * 5 + originY))
+        pen.addLine(to: CGPoint(x: cellSide * 8 + originX, y: cellSide * 5 + originY))
+
+        for i in 0..<9 {
+            pen.move(to: CGPoint(x: originX, y: cellSide * CGFloat(i) + originY))
+            pen.addLine(to: CGPoint(x: cellSide * 8 + originX, y: cellSide * CGFloat(i) + originY))
+        }
         
+        for i in 0..<2 {
+            pen.move(to: CGPoint(x: cellSide * 3 + originX, y: originY + cellSide * CGFloat(i * 7)))
+            pen.addLine(to: CGPoint(x: cellSide * 5 + originX, y: originY + cellSide * CGFloat(i * 7 + 2)))
+            
+            pen.move(to: CGPoint(x: cellSide * 5 + originX, y: originY + cellSide * CGFloat(i * 7)))
+            pen.addLine(to: CGPoint(x: cellSide * 3 + originX, y: originY + cellSide * CGFloat(i * 7 + 2)))
+        }
         pen.stroke()
     }
  
