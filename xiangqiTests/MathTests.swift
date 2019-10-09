558a7aa89a884f6240d16a18d7efeb034b86e443
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-10-08 20:49:29 -0400

    I solved ABCDE * 4 = EDCBA (Answer : EDCBA = 87912

---
+++ xiangqiTests/MathTests.swift
@@ -0,0 +1,49 @@
+//
+//  MathTests.swift
+//  xiangqiTests
+//
+//  Created by Angie Yan on 2019-10-08.
+//  Copyright © 2019 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+
+class MathTests: XCTestCase {
+
+    func testMath() {
+        /*
+ 
+         ABCDE * 4 = EDCBA
+         
+         A = 2 √
+         E = 8 √
+         B = 1 √
+         D = 7 √
+         C = 9
+         
+          333
+         21C78
+         *   4
+         -------
+         87C12
+         
+          333
+         21978
+         *   4
+         -------
+         87912
+         
+         333
+         21478
+         *   4
+         -------
+         87412
+         
+         
+         
+         */
+        
+    
+    }
+
+}
