c6ac381a0d31abb5ed0014acb195c091afc00804
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2020-05-28 15:46:16 -0400

    Hi

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -14,20 +14,80 @@ class BoardView: UIView {
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
         
-        for i in 0 ..< 10 {
+        for i in 0 ..< 9 {
             path.move(to: CGPoint(x: i * 50 + 77, y: 91))
-            path.addLine(to: CGPoint(x: i * 50 + 77, y: 500 + 91))
+            path.addLine(to: CGPoint(x: i * 50 + 77, y: 200 + 91))
+        }
+        
+        for i in 0 ..< 9 {
+            path.move(to: CGPoint(x: i * 50 + 77, y: 91 + 250))
+            path.addLine(to: CGPoint(x: i * 50 + 77, y: 450 + 91))
         }
 
-        for i in 0 ..< 11 {
+        for i in 0 ..< 10 {
             path.move(to: CGPoint(x: 77, y: i * 50 + 91))
-            path.addLine(to: CGPoint(x: 450 + 77, y: i * 50 + 91))
+            path.addLine(to: CGPoint(x: 400 + 77, y: i * 50 + 91))
         }
+        
+        path.move(to: CGPoint(x: 77, y: 91 + 200))
+        path.addLine(to: CGPoint(x: 77, y: 250 + 91))
+        
+        path.move(to: CGPoint(x: 77 + 400, y: 91 + 200))
+        path.addLine(to: CGPoint(x: 77 + 400, y: 250 + 91))
+        
+        path.move(to: CGPoint(x: 77 + 150, y: 91))
+        path.addLine(to: CGPoint(x: 77 + 250, y: 91 + 100))
+        
+        path.move(to: CGPoint(x: 77 + 250, y: 91))
+        path.addLine(to: CGPoint(x: 77 + 150, y: 100 + 91))
+        
+        path.move(to: CGPoint(x: 77 + 150, y: 91 + 450))
+        path.addLine(to: CGPoint(x: 77 + 250, y: 350 + 91))
+        
+        path.move(to: CGPoint(x: 77 + 250, y: 91 + 450))
+        path.addLine(to: CGPoint(x: 77 + 150, y: 350 + 91))
+        
+        
         
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         path.stroke()
         
+        
     }
     
-
+    func drawStar(x: 500, y: 200)
+    
+    
+    func drawHalfStarRight(x: CGFloat, y: CGFloat) {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: x + 25, y: y + 10))
+        path.addLine(to: CGPoint(x: x + 10, y: y + 10))
+        path.addLine(to: CGPoint(x: x + 10, y: y + 25))
+        path.move(to: CGPoint(x: x + 25, y: y - 10))
+        path.addLine(to: CGPoint(x: x + 10, y: y - 10))
+        path.addLine(to: CGPoint(x: x + 10, y: y - 25))
+        
+            path.stroke()
+    }
+    
+    func drawHalfStarLeft(x: CGFloat, y: CGFloat) {
+    let path = UIBezierPath()
+        
+        path.move(to: CGPoint(x: x - 25, y: y + 10))
+        path.addLine(to: CGPoint(x: x - 10, y: y + 10))
+        path.addLine(to: CGPoint(x: x - 10, y: y + 25))
+        path.move(to: CGPoint(x: x - 25, y: y - 10))
+        path.addLine(to: CGPoint(x: x - 10, y: y - 10))
+        path.addLine(to: CGPoint(x: x - 10, y: y - 25))
+        
+            path.stroke()
+    }
+    
+    func drawStar(x: CGFloat, y: CGFloat) {
+        drawHalfStarLeft(x: x, y: y)
+        drawHalfStarRight(x: x, y: y)
+        
+        
+    }
+    
 }
