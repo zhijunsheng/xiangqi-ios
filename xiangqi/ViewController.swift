3b2dcc625f6ca60499516bdae0caf5d913732432
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2020-06-22 10:14:43 -0400

    added all the chess pieces into the chessboard

--- xiangqi/ViewController.swift
+++ xiangqi/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Donald Sheng on 2018-03-02.
 //  Copyright © 2018 GoldThumb Inc. All rights reserved.
-//
+
 
 import UIKit
 
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//         Dispose of any resources that can be recreated.
     }
 
 
