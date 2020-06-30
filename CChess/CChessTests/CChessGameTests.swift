9288f1f1df14c135a5c1791ac6a70dc6bfeb474e
Zhijun Sheng <zhijunsheng@mbp2020.local>
2020-06-29 21:52:30 -0400

    tested a simple move of piece

--- CChess/CChessTests/CChessGameTests.swift
+++ CChess/CChessTests/CChessGameTests.swift
@@ -11,10 +11,21 @@ import XCTest
 
 class CChessGameTests: XCTestCase {
 
-    func testPrintingGameBoard() {
+    func testGameBoard() {
         let game = CChessGame()
-        game.reset()
-        print(game)
+        XCTAssertNotNil(game.pieceAt(col: 0, row: 0))
+        XCTAssertNil(game.pieceAt(col: 0, row: 1))
+    }
+    
+    func testMovePiece() {
+        let game = CChessGame()
+        XCTAssertNotNil(game.pieceAt(col: 0, row: 0))
+        XCTAssertNil(game.pieceAt(col: 0, row: 1))
+        
+        game.movePiece(fromCol: 0, fromRow: 0, toCol: 0, toRow: 1)
+        
+        XCTAssertNil(game.pieceAt(col: 0, row: 0))
+        XCTAssertNotNil(game.pieceAt(col: 0, row: 1))
     }
 
 }
