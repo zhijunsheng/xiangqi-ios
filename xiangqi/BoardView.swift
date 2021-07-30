538b0ca7091bc1cc96d322c1cf28ac5ea3eab0a7
吴淑雅 <wushuya@miyabis-MacBook-Air.local>
2021-07-30 17:24:39 -0400

    We made the "Background" longer, and we created another Chess Board.

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -10,27 +10,28 @@ import UIKit
 
 class BoardView: UIView {
 
-    
-
     override func draw(_ rect: CGRect) {
         
         let hL1 = UIBezierPath()
-        for i in 0..<5 {
+        for i in 0..<10 {
             hL1.move(to: CGPoint(x: 55, y: 75 + i * 80))
             hL1.addLine(to: CGPoint(x: 675, y: 75 + i * 80))
         }
         #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
         hL1.stroke()
         
+        let vL = UIBezierPath()
         for i in 0..<9 {
-            let vL = UIBezierPath()
             vL.move(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 75))
             vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 395))
-            #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
-            vL.stroke()
-
         }
-
+        for i in 0..<9 {
+            vL.move(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 475))
+            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 795))
+        }
+        #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
+        vL.stroke()
     }
+    
 
 }
