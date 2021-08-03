cd1709b9160ad339d247590f629bac923b28c67f
吴淑雅 <wushuya@miyabis-MacBook-Air.local>
2021-08-03 17:38:45 -0400

    Our top grid is very "flexible" and whatever value we assign to "cS", the top grid will not change it's shape.

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class BoardView: UIView {
 
+    let cS: CGFloat = 80 - 11
+    
     override func draw(_ rect: CGRect) {
         
         let hL1 = UIBezierPath()
         for i in 0..<10 {
-            hL1.move(to: CGPoint(x: 55, y: 75 + i * 80))
-            hL1.addLine(to: CGPoint(x: 675, y: 75 + i * 80))
+            hL1.move(to: CGPoint(x: 55, y: 75 + CGFloat(i) * cS))
+            hL1.addLine(to: CGPoint(x: 55 + 8 * cS, y: 75 + CGFloat(i) * cS))
         }
         #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
         hL1.stroke()
         
         let vL = UIBezierPath()
+        // Upper vertical lines are done
         for i in 0..<9 {
-            vL.move(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 75))
-            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 395))
+            vL.move(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 75))
+            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 75 + 4 * cS))
         }
-        for i in 0..<9 {
-            vL.move(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 475))
-            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * 77.5, y: 795))
+        // Lower vertical lines are not done
+        for i in 0..<1 {
+            vL.move(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 475))
+            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 795))
         }
         #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
         vL.stroke()
