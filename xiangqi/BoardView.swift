a7eab74824af0e266d5c93b5d76c04523a04c650
jichengye <jichengye@jichengyedeMacBook-Pro.local>
2021-08-04 09:17:26 +0800

    We finished the xiangqi board .

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -4,34 +4,50 @@
 //
 //  Created by jichengye on 2021/7/28.
 //  Copyright © 2021 GoldThumb Inc. All rights reserved.
-//
+
 
 import UIKit
 
 class BoardView: UIView {
 
+    var cellH: CGFloat = 48
+    var gridX: CGFloat = 172
+    var gridY: CGFloat = 210
+    
     override func draw(_ rect: CGRect) {
-            
-        let line1 = UIBezierPath()
-        for i in 0 ..< 9 {
-            line1.move(to: CGPoint(x: 50, y: 50 + 68.75 * CGFloat(i)))
-            line1.addLine(to: CGPoint(x: 50 + 68.75 * 8, y: 50 + 68.75 * CGFloat(i)))
-            
-        }
-        line1.stroke()
         
+        print(bounds.width)
+        print(bounds.height)
+        
+        gridX = (728 - 48 * 8) / 2
+        gridY = (852 - 48 * 9) / 2
+        
+        let line = UIBezierPath()
+        
+        for i in 0 ..< 9 {
+            line.move(to: CGPoint(x: gridX + cellH * CGFloat(i), y: gridY))
+            line.addLine(to: CGPoint(x: gridX + cellH * CGFloat(i), y: gridY + cellH * 4))
+            
+            line.move(to: CGPoint(x: gridX + cellH * CGFloat(i), y: gridY + cellH * 5))
+            line.addLine(to: CGPoint(x: gridX + cellH * CGFloat(i), y: gridY + cellH * 9))
+        }
+        
+        for i in 0 ..< 5 {
+            line.move(to: CGPoint(x: gridX, y: gridY + CGFloat(i) * cellH))
+            line.addLine(to: CGPoint(x: gridX + cellH * 8, y: gridY + CGFloat(i) * cellH))
+            
+            line.move(to: CGPoint(x: gridX, y: gridY + cellH * 5 + CGFloat(i) * cellH))
+            line.addLine(to: CGPoint(x: gridX + cellH * 8, y: gridY + cellH * 5 + CGFloat(i) * cellH))
+        }
+        
+        for i in 0 ..< 2 {
+            line.move(to: CGPoint(x: gridX + cellH * 3, y: gridY + cellH * CGFloat(i * 7)))
+            line.addLine(to: CGPoint(x: gridX + cellH * 5, y: gridY + cellH * CGFloat(i * 7 + 2)))
+            
+            line.move(to: CGPoint(x: gridX + cellH * 5, y: gridY + cellH * CGFloat(i * 7)))
+            line.addLine(to: CGPoint(x: gridX + cellH * 3, y: gridY + cellH * CGFloat(i * 7 + 2)))
+        }
+        
+        line.stroke()
     }
-    
-
 }
-
-
-  
-    
-
-
-
-
-
-
-
