c867fe4534ed2724d34c255b3d4dca101e48b8fc
吴淑雅 <wushuya@miyabis-MacBook-Air.local>
2021-08-06 16:50:15 -0400

    We fixed our Chinese chess board (on the bottom) and our Chinese chess board (The whole thing) is now very flexible.

--- xiangqi/BoardView.swift
+++ xiangqi/BoardView.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class BoardView: UIView {
-
-    let cS: CGFloat = 80 - 11
+    
+    let gridX: CGFloat = 90
+    let gridY: CGFloat = 115
+    let cellSide: CGFloat = 69 
     
     override func draw(_ rect: CGRect) {
         
         let hL1 = UIBezierPath()
         for i in 0..<10 {
-            hL1.move(to: CGPoint(x: 55, y: 75 + CGFloat(i) * cS))
-            hL1.addLine(to: CGPoint(x: 55 + 8 * cS, y: 75 + CGFloat(i) * cS))
+            hL1.move(to: CGPoint(x: gridX, y: gridY + CGFloat(i) * cellSide))
+            hL1.addLine(to: CGPoint(x: gridX + 8 * cellSide, y: gridY + CGFloat(i) * cellSide))
         }
         #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
         hL1.stroke()
@@ -25,13 +27,13 @@ class BoardView: UIView {
         let vL = UIBezierPath()
         // Upper vertical lines are done
         for i in 0..<9 {
-            vL.move(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 75))
-            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 75 + 4 * cS))
+            vL.move(to: CGPoint(x: gridX + CGFloat(i) * cellSide, y: gridY))
+            vL.addLine(to: CGPoint(x: gridX + CGFloat(i) * cellSide, y: gridY + 4 * cellSide))
         }
         // Lower vertical lines are not done
-        for i in 0..<1 {
-            vL.move(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 475))
-            vL.addLine(to: CGPoint(x: 55 + CGFloat(i) * cS, y: 795))
+        for i in 0..<9 {
+            vL.move(to: CGPoint(x: gridX + CGFloat(i) * cellSide, y: gridY + cellSide * 5))
+            vL.addLine(to: CGPoint(x: gridX + CGFloat(i) * cellSide, y: gridY + cellSide * 9))
         }
         #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1).setStroke()
         vL.stroke()
